1c99821e894e7c18e8ba8cdc3905de26405475aa
michelangelomancini <michelangelomancini8@gmail.com>
2025-01-17 15:54:10 +0100

    creazione card team con funzione e ouput su pagina

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "img/male3.png"
   },
   {
     name: "Laura Rossi",
@@ -44,5 +44,45 @@ console.table (teamMembers);
 // richiamo la variabile di output
 const cardOutput= document.getElementById('outputCardElement');
 
-// 
+
+// setto la variabile items (vuota)
+let items = "";
+
+// itero la lista del team per creare output
+for (let i = 0; i < teamMembers.length; i++) {
+   
+   items += createCardMember(teamMembers[i]);
+}
+
+// funzione per creare card team
+function createCardMember(cardTeam) {
+
+  // destrutturazione oggetto
+  const { name, role, email, img } = cardTeam;
+
+  // creazione card-team
+  let card = `   
+   <div class="container text-center" class="row align-items-start"> 
+    <div class="card-team" class="col">
+    
+      <div>
+          <img src="${img}" alt="${name}" />
+      </div>
+
+      <div>
+          <h3>${name}</h3>
+          <p>${email}</p>
+          <p>${role}</p>
+      </div>
+
+    </div>
+   </div>
+
+  `
+  return card
+}
+
+// output in pagina
+cardOutput.innerHTML = items;
+
 
